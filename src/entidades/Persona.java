f246d560d64d58b39f98f52d38390cd9ce0426c4
Sammier1821 <productive.gian.112020@gmail.com>
2022-12-01 13:52:40 -0500

    First commit

---
+++ src/entidades/Persona.java
@@ -0,0 +1,50 @@
+package entidades;
+
+public class Persona {
+  protected String dni;
+  protected String nombres;
+  protected String apellidos;
+  
+  public Persona() {
+    dni = "12345678";
+    nombres = "NN";
+    apellidos = "NA";
+  }
+
+  public Persona(String dni, String apellidos, String nombres) {
+    this.dni = dni;
+    this.nombres = nombres;
+    this.apellidos = apellidos;
+  }
+
+  public void setDni(String dni) {
+    this.dni = dni;
+  }
+
+  public void setNombres(String nombres) {
+    this.nombres = nombres;
+  }
+
+  public void setApellidos(String apellidos) {
+    this.apellidos = apellidos;
+  }
+
+  public String getDni() {
+    return dni;
+  }
+
+  public String getNombres() {
+    return nombres;
+  }
+
+  public String getApellidos() {
+    return apellidos;
+  }
+
+  @Override
+  public String toString() {
+    return "\nDNI: " + getDni() +
+            "\nNombres: " + getNombres() +
+            "\nApellidos: " + getApellidos();
+  }
+}
